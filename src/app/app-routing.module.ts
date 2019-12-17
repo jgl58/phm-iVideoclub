441e55a2955a6038557589dfbd6a329c819810c7
Jonay Gilabert López <jgl58@alu.ua.es>
2019-12-17 17:40:15 +0100

    Creadas primeras paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'autor',
+    loadChildren: () => import('./autor/autor.module').then( m => m.AutorPageModule)
+  },
+  {
+    path: 'curriculum',
+    loadChildren: () => import('./curriculum/curriculum.module').then( m => m.CurriculumPageModule)
+  },
+  {
+    path: 'videoclub',
+    loadChildren: () => import('./videoclub/videoclub.module').then( m => m.VideoclubPageModule)
+  },
 ];
 
 @NgModule({
